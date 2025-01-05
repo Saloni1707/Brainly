c45588eeca34fea918e8fc8f93f5d4f06b7f4650
Saloni Atole <145243379+Saloni1707@users.noreply.github.com>
2025-01-05 18:38:51 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import express from "express";
+import cors from "cors"; // Import cors
 import { random } from "./utils";
 import jwt from "jsonwebtoken";
 import { ContentModel, LinkModel, UserModel } from "./db";
 import { JWT_PASSWORD } from "./config";
 import { userMiddleware } from "./middleware";
 
+declare global {
+    export interface Request {
+        userId?: string;
+    }
+}
 
 const app = express();
-app.use(express.json());
+
+// Use the CORS middleware
 app.use(cors());
 
+app.use(express.json());
+
 app.post("/api/v1/signup", async (req, res) => {
-    // TODO: zod validation , hash the password
     const username = req.body.username;
     const password = req.body.password;
 
@@ -19,17 +27,17 @@ app.post("/api/v1/signup", async (req, res) => {
         await UserModel.create({
             username: username,
             password: password
-        }) 
+        });
 
         res.json({
             message: "User signed up"
-        })
-    } catch(e) {
+        });
+    } catch (e) {
         res.status(411).json({
             message: "User already exists"
-        })
+        });
     }
-})
+});
 
 app.post("/api/v1/signin", async (req, res) => {
     const username = req.body.username;
@@ -38,21 +46,25 @@ app.post("/api/v1/signin", async (req, res) => {
     const existingUser = await UserModel.findOne({
         username,
         password
-    })
+    });
+
     if (existingUser) {
-        const token = jwt.sign({
-            id: existingUser._id
-        }, JWT_PASSWORD)
+        const token = jwt.sign(
+            {
+                id: existingUser._id
+            },
+            JWT_PASSWORD
+        );
 
         res.json({
             token
-        })
+        });
     } else {
         res.status(403).json({
-            message: "Incorrrect credentials"
-        })
+            message: "Incorrect credentials"
+        });
     }
-})
+});
 
 app.post("/api/v1/content", userMiddleware, async (req, res) => {
     const link = req.body.link;
@@ -61,72 +73,76 @@ app.post("/api/v1/content", userMiddleware, async (req, res) => {
         link,
         type,
         title: req.body.title,
+        //@ts-ignore
         userId: req.userId,
         tags: []
-    })
+    });
 
     res.json({
         message: "Content added"
-    })
-    
-})
+    });
+});
 
 app.get("/api/v1/content", userMiddleware, async (req, res) => {
-    // @ts-ignore
+    //@ts-ignore
     const userId = req.userId;
     const content = await ContentModel.find({
         userId: userId
-    }).populate("userId", "username")
+    }).populate("userId", "username");
     res.json({
         content
-    })
-})
+    });
+});
 
 app.delete("/api/v1/content", userMiddleware, async (req, res) => {
     const contentId = req.body.contentId;
 
     await ContentModel.deleteMany({
         contentId,
+        //@ts-ignore
         userId: req.userId
-    })
+    });
 
     res.json({
         message: "Deleted"
-    })
-})
+    });
+});
 
 app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
     const share = req.body.share;
     if (share) {
-            const existingLink = await LinkModel.findOne({
-                userId: req.userId
-            });
+        const existingLink = await LinkModel.findOne({
+            //@ts-ignore
+            userId: req.userId
+        });
 
-            if (existingLink) {
-                res.json({
-                    hash: existingLink.hash
-                })
-                return;
-            }
-            const hash = random(10);
-            await LinkModel.create({
-                userId: req.userId,
-                hash: hash
-            })
-
+        if (existingLink) {
             res.json({
-                hash
-            })
+                hash: existingLink.hash
+            });
+            return;
+        }
+        const hash = random(10);
+        await LinkModel.create({
+            //@ts-ignore
+            userId: req.userId,
+            hash: hash
+        });
+
+        res.json({
+            hash
+        });
     } else {
         await LinkModel.deleteOne({
+            //@ts-ignore
             userId: req.userId
         });
 
         res.json({
             message: "Removed link"
-        })
+        });
     }
-})
+});
 
 app.get("/api/v1/brain/:shareLink", async (req, res) => {
     const hash = req.params.shareLink;
@@ -137,32 +153,33 @@ app.get("/api/v1/brain/:shareLink", async (req, res) => {
 
     if (!link) {
         res.status(411).json({
-            message: "Sorry incorrect input"
-        })
+            message: "Sorry, incorrect input"
+        });
         return;
     }
-    // userId
+
     const content = await ContentModel.find({
         userId: link.userId
-    })
+    });
 
     console.log(link);
     const user = await UserModel.findOne({
         _id: link.userId
-    })
+    });
 
     if (!user) {
         res.status(411).json({
-            message: "user not found, error should ideally not happen"
-        })
+            message: "User not found, error should ideally not happen"
+        });
         return;
     }
 
     res.json({
         username: user.username,
         content: content
-    })
+    });
+});
 
-})
-
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
